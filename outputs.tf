127b7d0375cd6b3ce4bbbc5da06e44b7e6f3075b
lechuk1981 <lechuk@yandex.ru>
2023-11-24 23:06:12 +0300

    Update outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,7 @@
+output "instance_first_vm" {
+  value = "${var.vm_web_name}   ${yandex_compute_instance.platform.network_interface[0].nat_ip_address}"
+}
+
+output "instance_second_vm" {
+  value = "${var.vm_db_name}    ${yandex_compute_instance.platform_db.network_interface[0].nat_ip_address}"
+}
