578262022f3ff6cc50daa759a05188471985ab31
lechuk1981 <lechuk@yandex.ru>
2023-12-03 23:43:50 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,47 +1,22 @@
-#terraform {
-#  required_providers {
-#    yandex = {
-#      source = "yandex-cloud/yandex"
-#    }
-#  }
-#  required_version = ">=0.13"
-#}
-#
-#provider "yandex" {
-#  token     = var.token
-#  cloud_id  = var.cloud_id
-#  folder_id = var.folder_id
-#  zone      = var.default_zone
-#}
-#
-#
-
-#создаем облачную сеть
-resource "yandex_vpc_network" "develop" {
-  name = "develop"
+module "vpc" {
+  source	= "./vpc"
+  env_name  = "develop"
+  zone  	= "ru-central1-a"
+  cidr  	= "10.0.1.0/24"
 }
-
-#создаем подсеть
-resource "yandex_vpc_subnet" "develop" {
-  name           = "develop-ru-central1-a"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["10.0.1.0/24"]
-}
-
 module "test-vm" {
   source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
   env_name        = "develop"
-  network_id      = yandex_vpc_network.develop.id
+  network_id      = module.vpc.vpc_id
   subnet_zones    = ["ru-central1-a"]
-  subnet_ids      = [ yandex_vpc_subnet.develop.id ]
+  subnet_ids      = [module.vpc.subnet_id]
   instance_name   = "web"
-  instance_count  = 2
+  instance_count  = 1
   image_family    = "ubuntu-2004-lts"
   public_ip       = true
-  
-  metadata = {
-      user-data         	= data.template_file.cloudinit.rendered
+
+ metadata = {
+      user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
       serial-port-enable = 1
   }
 
@@ -49,10 +24,8 @@ module "test-vm" {
 
 #Пример передачи cloud-config в ВМ для демонстрации №3
 data template_file "cloudinit" {
-
-  template = file("${path.module}/cloud-init.yml")
-        vars = {
-	    ssh_public_key = file("~/.ssh/id_ed25519.pub")
+ template = file("./cloud-init.yml")
+  vars = {
+	ssh_public_key = local.ssh-key
   }
 }
-
